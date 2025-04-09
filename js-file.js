bee3ea35665124c121323ea81c1a865febcde4ab
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-09 17:24:13 +0530

    Creates calculator layout similr to student project
    Created as a step towards a fully functional calc.

--- js-file.js
+++ js-file.js
@@ -1,46 +1,27 @@
 let mainContainer=document.querySelector("#main-container");
-let blueBtns=document.querySelector("#blue-buttons");
-let greenBtns=document.querySelector("#green-buttons");
-let orangeBtns=document.querySelector("#orange-buttons");
-let operatorSymbols=["AC", "+/-", "%", "/", "*", "-", "+", "=", "."];
+let btns=document.querySelector("#buttons");
+let calcSymbols=["AC", "+/-", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+","0", ".", "="];
 
 
-let blueBtnArray=[];
-for (let i=0;i<3;i++){
-    blueBtnArray.push(document.createElement("button"));
-    blueBtns.appendChild(blueBtnArray[i]);
-    blueBtnArray[i].style.width="33%";
-    blueBtnArray[i].style.height="95%";
-    blueBtnArray[i].style.backgroundColor="rgb(30, 144, 255)";
-    blueBtnArray[i].style.borderRadius="5px";
-    blueBtnArray[i].style.padding="auto";
-    blueBtnArray[i].textContent=operatorSymbols[i];
+let btnArray=[];
+for (let i=0;i<19;i++){
+    btnArray.push(document.createElement("button"));
+    btns.appendChild(btnArray[i]);
+    btnArray[i].style.width="24%";
+    btnArray[i].style.height="19%";
+    btnArray[i].style.borderRadius="5px";
+    btnArray[i].style.borderWidth="0px";
+    btnArray[i].style.padding="auto";
+    btnArray[i].textContent=calcSymbols[i];
+    if (i==16){
+        btnArray[i].style.width="49%"; 
+    }
+    if((i+1)%4==0){
+        btnArray[i].style.backgroundColor="#FF8F1F";
+    }else if(i<3){
+        btnArray[i].style.backgroundColor="rgb(30, 144, 255)";
+    }else{
+        btnArray[i].style.backgroundColor="rgb(33, 124, 126)";
+    }
 }
-
-let orangeBtnArray=[];
-for (let i=0;i<5;i++){
-    orangeBtnArray.push(document.createElement("button"));
-    orangeBtns.appendChild(orangeBtnArray[i]);
-    orangeBtnArray[i].style.width="95%";
-    orangeBtnArray[i].style.height="20%";
-    orangeBtnArray[i].style.backgroundColor="rgb(30, 144, 255)";
-    orangeBtnArray[i].style.borderRadius="5px";
-    orangeBtnArray[i].style.padding="auto";
-    orangeBtnArray[i].textContent=operatorSymbols[i];
-}
-
-
-
-// for (let i=0; i<19; i++){
-//     btnArray.push(document.createElement("button"));
-//     btnArray[i].style.width="25%";
-//     btnArray[i].style.Height="16%";
-// }
-// btnArray[16].style.flexGrow="2";
-
-// btnArray[0].textContent="AC";
-
-
-// for (let btns of btnArray){
-//     mainContainer.appendChild(btns);
-// }
+btnArray[18].style.backgroundColor="#FF8F1F";
